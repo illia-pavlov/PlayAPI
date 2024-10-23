500c8caf00a6ef6ccc4f9d032c2f51f4d1798e89
illia-pavlov <85336655+illia-pavlov@users.noreply.github.com>
2024-10-23 17:50:40 +0300

    add CI/CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,24 @@
-import { PlaywrightTestConfig } from "@playwright/test";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const config: PlaywrightTestConfig = {
-  timeout: 60000,
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./src/tests",
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "html",
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: process.env.API_BASE_URL || "https://reqres.in",
+    trace: "on-first-retry",
   },
-  reporter: [["html", { outputFolder: "test-results", open: "never" }]],
-};
-
-export default config;
+});
